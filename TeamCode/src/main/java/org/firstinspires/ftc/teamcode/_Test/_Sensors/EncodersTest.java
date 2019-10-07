c0957871a3cf9e4a56343aa0eb4aaacd164612b8
Paul Hanau <phanau@comcast.net>
2019-10-07 13:24:07 -0700

    EncodersTest: add opmode that displays readings from motor encoders with a given set of motor names.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_Test/_Sensors/EncodersTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode._Test._Sensors;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode._Libs.AutoLib;
+
+
+/**
+ * Created by phanau on 10/7/19.
+ * Test motor encoders connected to encoder inputs of a RevHub
+ */
+@Autonomous(name="Test: Encoders Test", group ="Test")
+//@Disabled
+public class EncodersTest extends OpMode {
+
+    DcMotor mMotors[];
+    final String motorNames[] = { "fl", "bl", "fr", "br"};
+
+    public EncodersTest() {
+    }
+
+    public void init() {
+        // get hardware
+        AutoLib.HardwareFactory mf = new AutoLib.RealHardwareFactory(this);
+        mMotors = new DcMotor[motorNames.length];
+        for (int i = 0; i < motorNames.length; i++) {
+            mMotors[i] = mf.getDcMotor(motorNames[i]);
+            mMotors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+    }
+
+    public void loop() {
+        // log data to DriverStation
+        for (int i = 0; i < mMotors.length; i++)
+            telemetry.addData(motorNames[i], mMotors[i].getCurrentPosition());
+    }
+
+    public void stop() {}
+
+}
